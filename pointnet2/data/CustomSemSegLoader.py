d112d9a769e43d95b97f3d6b0dcbaefab7528776
Bela Bohlender <bbohlender@coconut-capital.com>
2021-03-13 22:32:17 +0100

    shuffle points

--- pointnet2/data/CustomSemSegLoader.py
+++ pointnet2/data/CustomSemSegLoader.py
@@ -39,9 +39,11 @@ class Custom3DSemSeg(data.Dataset):
             self.labels = label_batchlist[:training_amount]
 
     def __getitem__(self, idx):
+        pt_idxs = np.arange(0, self.num_points)
+        np.random.shuffle(pt_idxs)
 
-        current_points = torch.from_numpy(self.points[idx].copy()).float()
-        current_labels = torch.from_numpy(self.labels[idx].copy()).long()
+        current_points = torch.from_numpy(self.points[idx, pt_idxs].copy()).float()
+        current_labels = torch.from_numpy(self.labels[idx, pt_idxs].copy()).long()
 
         return current_points, current_labels
 
