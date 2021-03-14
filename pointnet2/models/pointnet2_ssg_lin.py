d4381b77e81c0dc6262f1f63981d0d7fb8fd8d64
Bela Bohlender <bbohlender@coconut-capital.com>
2021-03-14 13:32:57 +0100

    fix ssg_lin

--- pointnet2/models/pointnet2_ssg_lin.py
+++ pointnet2/models/pointnet2_ssg_lin.py
@@ -10,50 +10,32 @@ from pointnet2.models.pointnet2_ssg_cls import PointNet2ClassificationSSG
 
 
 class PointNet2LinearSSG(PointNet2ClassificationSSG):
+
     def _build_model(self):
         self.SA_modules = nn.ModuleList()
         self.SA_modules.append(
             PointnetSAModule(
-                npoint=1024,
-                radius=0.1,
-                nsample=32,
-                mlp=[0, 32, 32, 64],
-                use_xyz=self.hparams["model.use_xyz"],
-            )
-        )
-        self.SA_modules.append(
-            PointnetSAModule(
-                npoint=256,
+                npoint=512,
                 radius=0.2,
-                nsample=32,
-                mlp=[64, 64, 64, 128],
+                nsample=64,
+                mlp=[0, 64, 64, 128],
                 use_xyz=self.hparams["model.use_xyz"],
             )
         )
         self.SA_modules.append(
             PointnetSAModule(
-                npoint=64,
+                npoint=128,
                 radius=0.4,
-                nsample=32,
+                nsample=64,
                 mlp=[128, 128, 128, 256],
                 use_xyz=self.hparams["model.use_xyz"],
             )
         )
         self.SA_modules.append(
             PointnetSAModule(
-                npoint=16,
-                radius=0.8,
-                nsample=32,
-                mlp=[256, 256, 256, 512],
-                use_xyz=self.hparams["model.use_xyz"],
+                mlp=[256, 256, 512, 1024], use_xyz=self.hparams["model.use_xyz"]
             )
         )
-
-        self.FP_modules = nn.ModuleList()
-        self.FP_modules.append(PointnetFPModule(mlp=[128 + 0, 128, 128, 128]))
-        self.FP_modules.append(PointnetFPModule(mlp=[256 + 64, 256, 128]))
-        self.FP_modules.append(PointnetFPModule(mlp=[256 + 128, 256, 256]))
-        self.FP_modules.append(PointnetFPModule(mlp=[512 + 256, 256, 256]))
 
         self.fc_layer = nn.Sequential(
             nn.Linear(1024, 512, bias=False),
@@ -63,7 +45,7 @@ class PointNet2LinearSSG(PointNet2ClassificationSSG):
             nn.BatchNorm1d(256),
             nn.ReLU(True),
             nn.Dropout(0.5),
-            nn.Linear(256, 1),
+            nn.Linear(256, self.hparams["classes"]),
         )
 
     def training_step(self, batch, batch_idx):
@@ -108,7 +90,7 @@ class PointNet2LinearSSG(PointNet2ClassificationSSG):
             xyz, features = module(xyz, features)
 
         return self.fc_layer(features.squeeze(-1))
-        
+
     def prepare_data(self):
         self.train_dset = Custom3DLinear(self.hparams["batch_dir"], self.hparams["batch_file"], self.hparams["num_points"], train=True)
         self.val_dset = Custom3DLinear(self.hparams["batch_dir"], self.hparams["batch_file"], self.hparams["num_points"], train=False)
